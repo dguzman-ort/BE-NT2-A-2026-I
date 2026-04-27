52dc778002b354e039b2293cc4b20d3710f980bf
Sofia Olguin <sofiaolguin@gmail.com>
2026-04-27 14:51:47 -0300

    ejercicios y practica

--- React/todo-app/src/App.jsx
+++ React/todo-app/src/App.jsx
@@ -1,14 +1,41 @@
 import './App.css'
+import { useState } from 'react'
+import TodoItem from './components/TodoItem'
 
 function App() {
+
+const [tareas, setTareas] = useState([
+    { id: 1, texto: ' lol', completada: false },
+    { id: 2, texto: 'Revisar correo', completada: false }
+  ]);
+
+  // LÓGICA 1: Eliminar
+  const eliminarTarea = (id) => {
+    const nuevasTareas = tareas.filter(t => t.id !== id);
+    setTareas(nuevasTareas);
+  };
+
+  // LÓGICA 2: Marcar como completada/pendiente
+  const toggleTarea = (id) => {
+    const tareasActualizadas = tareas.map(t => 
+      t.id === id ? { ...t, completada: !t.completada } : t
+    );
+    setTareas(tareasActualizadas);
+  };
+
+  // LÓGICA 3: Cálculos para los contadores
+  const total = tareas.length;
+  const completadas = tareas.filter(t => t.completada).length;
+
+
   return (
     <>
       <div className="container center">
-        <h1 className="center title">TODO App</h1>
+        <h1 className="center title">TO-DO App</h1>
 
         <div className="todo-stats" role="status" aria-live="polite">
-          <span>Total de tareas: 2</span>
-          <span>Tareas completadas: 1</span>
+          <span>Total de tareas: {total}</span>
+          <span>Tareas completadas: {completadas}</span>
         </div>
         <div className="controls flow-right">
           <input type="text" placeholder="Nueva tarea" aria-label="Título de la tarea" />
@@ -17,14 +44,15 @@ function App() {
           </button>
         </div>
 
+
         <ul id="todoList" className="todo-list">
-          <li className="todo-container" data-id="1">
+         {/*  <li className="todo-container" data-id="1">
             <div className="flow-right">
               <input
                 type="checkbox"
                 className="todo-checkbox"
                 defaultChecked
-                aria-label="Completada: Comprar leche"
+                aria-label="Completada: xd leche"
               />
               <span>Comprar leche</span>
               <button type="button" className="button todo-delete">
@@ -44,11 +72,21 @@ function App() {
                 Eliminar
               </button>
             </div>
-          </li>
+          </li> */}
+          {tareas.map(t => (
+          <TodoItem 
+            key={t.id} 
+            tarea={t} 
+            onDelete={eliminarTarea} 
+            onToggle={toggleTarea} 
+          />
+        ))}
+          
         </ul>
       </div>
     </>
-  )
+  
+)
 }
 
 export default App
